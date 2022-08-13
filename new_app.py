0aeb5356faf7c7ee42dea0f0cce3b83fed4bbed8
NetserBr <netserbr@gmail.com>
2022-08-13 22:08:32 +0300

    Small changes

--- new_app.py
+++ new_app.py
@@ -51,10 +51,10 @@ class User(db.Model, UserMixin):
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+                           InputRequired(), Length(min=0, max=20)], render_kw={"placeholder": "Username"})
 
     password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+                             InputRequired(), Length(min=0, max=20)], render_kw={"placeholder": "Password"})
 
     submit = SubmitField('Login')
 
